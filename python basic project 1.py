ccf9c59c5a572c5419f31438bbe3515a73554ef4
Nithin-av <nithinnithin0463@gmail.com>
2025-05-19 00:39:27 +0530

    Add files via upload

---
+++ python basic project 1.py
@@ -0,0 +1,23 @@
+# define the two numbers
+num1 = 10
+num2 = 5
+
+# Perform addition
+addition = num1 + num2
+print(f"Addition: {num1} + {num2} = {addition}")
+
+# Perform subtraction
+subtraction = num1 - num2
+print(f"Subtraction: {num1} - {num2} = {subtraction}")
+
+# Perform multiplication
+multiplication = num1 * num2
+print(f"Multiplication: {num1} * {num2} = {multiplication}")
+
+# Perform division 
+if num2 != 0:
+    division  = num1 / num2
+    print(f"Division: {num1} / {num2} ={division}")
+else:
+    print("Cannot divide by zero.")
+    
